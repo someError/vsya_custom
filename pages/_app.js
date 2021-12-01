fba2e5d0d00451bf3675ea6e6d7bdb513a98af6d
alexandr <koghuxov1990@gmail.com>
2021-12-01 16:43:28 +0300

    fixes

--- pages/_app.js
+++ pages/_app.js
@@ -56,6 +56,7 @@ function MyApp({Component, pageProps}) {
 
     return <>
         <Head>
+            <title>vsya.store - ручная роспись, кастом одежды</title>
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
