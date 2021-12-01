9c3ac95b0c90177589915efee7ad6caa41d24a3d
alexandr <koghuxov1990@gmail.com>
2021-12-01 16:36:04 +0300

    fixes

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,8 @@ import '../public/assets/fonts/stylesheet.css'
 import {AnimateSharedLayout} from "framer-motion";
 import styles from "./styles.module.css";
 import {FiInstagram, FiMail} from "react-icons/fi";
-import { MdOutlineDesktopAccessDisabled } from 'react-icons/md'
-import { AiOutlineMobile } from 'react-icons/ai'
+import {MdOutlineDesktopAccessDisabled} from 'react-icons/md'
+import {AiOutlineMobile} from 'react-icons/ai'
 import Footer from "../components/footer";
 import {useState, useContext, createContext, useEffect} from "react";
 import {useSwipeable} from "react-swipeable";
@@ -14,14 +14,14 @@ import Head from 'next/head'
 import {useRouter} from "next/router";
 import i18n from "../i18n";
 import dynamic from 'next/dynamic'
-import { ScreenRotation } from "../components/devCup";
+import {ScreenRotation} from "../components/devCup";
 import useReactSimpleMatchMedia from 'react-simple-matchmedia'
 
-const DeviceOrientation = dynamic(() => import('react-screen-orientation'), { ssr: false })
-const Orientation = dynamic(() => import('react-screen-orientation').then((module) => module.Orientation), { ssr: false })
+const DeviceOrientation = dynamic(() => import('react-screen-orientation'), {ssr: false})
+const Orientation = dynamic(() => import('react-screen-orientation').then((module) => module.Orientation), {ssr: false})
 
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
     const router = useRouter()
     const tt = i18n[router.locale]
     const [footer, setFooter] = useState(false);
@@ -30,7 +30,6 @@ function MyApp({ Component, pageProps }) {
     const [curItemIndex, setCurItemIndex] = useState(2);
     const [isMobile, setIsMobile] = useState(true);
     const tabletMatched = useReactSimpleMatchMedia('(min-width: 767px)');
-    console.log(tt, 'tt')
 
     useEffect(() => {
         if (window.orientation !== undefined) {
@@ -55,52 +54,61 @@ function MyApp({ Component, pageProps }) {
         onSwipedDown: () => setFooter(false)
     });
 
-    return  <>
+    return <>
         <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+            <meta name="msapplication-TileColor" content="#da532c"/>
+            <meta name="theme-color" content="#ffffff"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
         </Head>
         <Context.Provider value={contextState}>
             <div className={styles.component} {...handlers}>
-                <Header />
+                <Header/>
 
                 <DeviceOrientation lockOrientation={'portrait'}>
                     <Orientation orientation={'landscape'}>
-                        { isMobile ? <ScreenRotation /> : <NdPhone tt={tt} /> }
+                        {isMobile ? <ScreenRotation/> : <NdPhone tt={tt}/>}
                     </Orientation>
                     <Orientation orientation='portrait'>
                         {
-                            tabletMatched ? <NdPhone tt={tt} /> : <Component {...pageProps} />
+                            tabletMatched ? <NdPhone tt={tt}/> : <Component {...pageProps} />
                         }
 
                     </Orientation>
                 </DeviceOrientation>
 
-                <Footer active={contextState.footerContext || footer} setActive={setFooter}><div className={styles.wrapper}>
-                    <a href="https://www.instagram.com/vsya.custom" target={'_blank'} className={styles.footerRow}>
-                        <FiInstagram></FiInstagram>
-                        <div className={styles.footerRowInst}>
-                            <div>vsya.custom</div>
-                            <div>instagram.com</div>
-                        </div>
-                    </a>
-                    <a href="mailto:vsya.custom@gmail.com" target={'_blank'} className={styles.footerRow}>
-                        <FiMail></FiMail>
-                        <div className={styles.footerRowInst}>
-                            <div>vsya.custom@gmail.com</div>
-                            <div>{tt.mail}</div>
-                        </div>
-                    </a>
-                </div></Footer>
+                <Footer active={contextState.footerContext || footer} setActive={setFooter}>
+                    <div className={styles.wrapper}>
+                        <a href="https://www.instagram.com/vsya.custom" target={'_blank'} className={styles.footerRow}>
+                            <FiInstagram></FiInstagram>
+                            <div className={styles.footerRowInst}>
+                                <div>vsya.custom</div>
+                                <div>instagram.com</div>
+                            </div>
+                        </a>
+                        <a href="mailto:vsya.custom@gmail.com" target={'_blank'} className={styles.footerRow}>
+                            <FiMail></FiMail>
+                            <div className={styles.footerRowInst}>
+                                <div>vsya.custom@gmail.com</div>
+                                <div>{tt.mail}</div>
+                            </div>
+                        </a>
+                    </div>
+                </Footer>
             </div>
         </Context.Provider>
     </>
 }
 
-function NdPhone ({tt}) {
+function NdPhone({tt}) {
     return <div className={styles.ndPhone}>
-        <MdOutlineDesktopAccessDisabled />
+        <MdOutlineDesktopAccessDisabled/>
         <div className={styles.ndPhoneContent}>{tt && tt.devTitle1}</div>
-        <AiOutlineMobile />
+        <AiOutlineMobile/>
         <div className={styles.ndPhoneContent}>{tt && tt.devTitle2}</div>
     </div>
 }
